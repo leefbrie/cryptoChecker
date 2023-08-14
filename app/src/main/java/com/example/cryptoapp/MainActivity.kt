f00d3b8ab11a6e1f5c590d2c2846e4473c017f16
leefbrie <leefbrie@gmail.com>
2023-08-14 22:24:50 +0200

    start of crypto app

---
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.cryptoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cryptoapp.ui.theme.CryptoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+
+@Composable
+fun cryptoCard(){
+    
+}
+
+
