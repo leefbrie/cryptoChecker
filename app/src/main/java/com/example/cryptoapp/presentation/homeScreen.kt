b6b1a67d3cabba57a37fa93c70226386808271ca
leefbrie <leefbrie@gmail.com>
2023-10-17 13:14:00 +0200

    added homescreen

---
+++ app/src/main/java/com/example/cryptoapp/presentation/homeScreen.kt
@@ -0,0 +1,82 @@
+package com.example.cryptoapp.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.cryptoapp.R
+import com.example.cryptoapp.Screen
+
+@Composable
+fun MainScreen(
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CryptoCard("poep", "poep,",  16 ,  navController)
+    }
+}
+
+@Composable
+fun CryptoCard(
+    item: String,
+    symbol: String,
+    icon: Int,
+    navController: NavController
+
+) {
+    Card(
+        shape = RoundedCornerShape(8.dp), modifier = Modifier.fillMaxWidth(0.8f)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.White),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Image(
+                painter = painterResource(id = icon),
+                contentDescription = "crypto icon",
+                modifier = Modifier.size(48.dp)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(
+                    text = item,
+                    color = colorResource(id = R.color.black),
+                )
+
+                Text(
+                    text = symbol,
+                    color = colorResource(id = R.color.black),
+                )
+            }
+        }
+
+        Button(onClick = {
+            navController.navigate(Screen.AlertScreen.route)
+        }) {
+            Text(text = "Move to Next Screen")
+        }
+    }
+}
